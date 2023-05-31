2c5fe8fd0edf030b43515203b7015ff5dd022155
Nicole Tan <geniusnic009@gmail.com>
2023-05-31 14:07:39 +0800

    added background, text, and image

--- group project/NicoleView.swift
+++ group project/NicoleView.swift
@@ -9,7 +9,45 @@ import SwiftUI
 
 struct NicoleView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color(.systemTeal)
+                
+            VStack {
+                Text("Hello!")
+                    .font(.largeTitle)
+                Text("My name is **Nicole**")
+                    .font(.title)
+                Image("penguin")
+                    .resizable()
+                    .scaledToFit()
+                    .saturation(5)
+                    .cornerRadius(20)
+                Text("I like **penguins** and the colour **blue** :D")
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+                
+                
+                
+            HStack {
+                                        
+                Link(destination: URL(string: "https://tk.sg/importantlink")!) {
+                    Image(systemName: "link.circle")
+                    }
+                    Spacer()
+                Link(destination: URL(string: "mailto:yjsoon@tk.sg")!) {
+                    Image(systemName: "envelope")
+                    }
+                    Spacer()
+                Image(systemName: "curlybraces.square")
+                                    }
+            .font(.title)
+            .foregroundColor(.black)
+            }
+            .padding()
+            .background(.white)
+            .cornerRadius(25)
+            .shadow(radius: 10)
+        }
     }
 }
 
