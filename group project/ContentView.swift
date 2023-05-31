271fba9288cc377946ab5ecabc2c061234ee01c3
Nicole Tan <geniusnic009@gmail.com>
2023-05-31 13:40:21 +0800

    Initial Commit

---
+++ group project/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  group project
+//
+//  Created by T Krobot on 31/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ group project/group_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  group_projectApp.swift
+//  group project
+//
+//  Created by T Krobot on 31/5/23.
+//
+
+import SwiftUI
+
+@main
+struct group_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
